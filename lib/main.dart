576c0c5bbb5db2a11fe2555f881c1637e3f20c84
Akshun <akshunjain4@gmail.com>
2026-07-14 01:59:48 +0530

    feat: setup feature-first architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:remind_circle/app/app.dart';
 import 'package:remind_circle/firebase_options.dart';
 
@@ -8,5 +9,5 @@ Future<void> main() async {
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const RemindCircleApp());
+  runApp(const ProviderScope(child: RemindCircleApp()));
 }
