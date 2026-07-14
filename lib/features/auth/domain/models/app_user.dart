d63a9dd8abced3a52b1ef47168cb3af569afa048
Akshun <akshunjain4@gmail.com>
2026-07-14 18:43:41 +0530

    feat: add authentication architecture

---
+++ lib/features/auth/domain/models/app_user.dart
@@ -0,0 +1,13 @@
+class AppUser {
+  final String uid;
+  final String name;
+  final String email;
+  final String? photoUrl;
+
+  const AppUser({
+    required this.uid,
+    required this.name,
+    required this.email,
+    this.photoUrl,
+  });
+}
